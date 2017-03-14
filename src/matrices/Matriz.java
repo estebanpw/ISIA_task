d87fc9b3d8167937bb31b7ab8f8fd3803e9941e1
Sergio Díaz del Pino <sergiodiazdp@gmail.com>
2017-03-14 19:59:19 +0100

    Fixed Inverse

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -21,11 +21,27 @@ public class Matriz {
         datos = new int[columnas][];
         for(int i=0; i<columnas; i++){
             datos[i] = new int[filas];
-            if (inicializarAleatorio)
-                for(int j=0; j<filas; j++)
-                    datos[i][j] = rnd.nextInt(100);
+        }
+                for(int i=0; i<columnas; i++){
+            datos[i] = new int[filas];
+            if (inicializarAleatorio){
+                datos[0][0] = 2;
+                datos[0][2] = 1;
+                datos[1][0] = 3;
+                datos[2][0] = 5;
+                datos[2][1] = 1;
+                datos[2][2] = 1; 
+            }
         }
     }
+    public Matriz(int datos[][]){
+        this.datos = datos;
+    }
+    
+    public void set_value_at(int i, int j, int value){
+        this.datos[i][j] = value;
+    }
+    
     public Matriz(Dimension d, boolean inicializarAleatorio){
         this(d.height, d.width, inicializarAleatorio);
     }
@@ -49,32 +65,45 @@ public class Matriz {
     } 
 
     public static Matriz matrizInversa(Matriz a) throws DimensionesIncompatibles {
-        if(! a.getDimension().height.equals(a.getDimension().width)) throw new DimensionesIncompatibles("La matriz debe ser cuadrada");
-        double det=1/determinante(a);
-        Matriz nmatriz=matrizAdjunta(a);
-        multiplicarMatriz(det,nmatriz);
-        return nmatriz;
+        if(a.getDimension().height != (a.getDimension().width)) throw new DimensionesIncompatibles("La matriz debe ser cuadrada");
+        
+        Matriz transMatriz = new Matriz(a.getDimension().height, a.getDimension().width, false);
+        
+        if( determinante(a) != 0 ){
+            double detInverso=1/determinante(a);
+            Matriz adjMatriz=matrizAdjunta(a);
+            transMatriz = matrizTranspuesta(adjMatriz);
+            transMatriz = multiplicarMatriz(detInverso, transMatriz);
+        }
+        
+        return transMatriz;
     }
 
-    public void multiplicarMatriz(double n, Matriz a) {
+    public static Matriz multiplicarMatriz(double n, Matriz a) {
         int i, j, dimensionA;
         dimensionA=a.getDimension().height;
-        for(i=0;i<dimensionA;i++)
-                for(j=0;j<dimensionA;j++)
-                        a.datos[i][j]*=n;
+        System.out.println("MultiPrincipio"+a.datos[0][0]);
+        for(i=0;i<dimensionA;i++){
+                for(j=0;j<dimensionA;j++){
+                        System.out.println("I: "+i+" J: "+j+" dato1: "+a.datos[i][j]+" n: "+n);
+                        a.datos[i][j]= (int) (a.datos[i][j]*n);
+                }
+        }
+        System.out.println("Multi"+a);
+        return a;
     }
  
     public static Matriz matrizAdjunta(Matriz a){
-        return matrizTranspuesta(matrizCofactores(a));
+        return matrizCofactores(a);
     }
  
-    public Matriz matrizCofactores(Matriz a){
+    public static Matriz matrizCofactores(Matriz a){
         int i, j, k, l, dimensionA;
         dimensionA=a.getDimension().height;
         Matriz nm = new Matriz(dimensionA, dimensionA, false);
         for(i=0;i<dimensionA;i++) {
             for(j=0;j<dimensionA;j++) {
-                double[][] det=new double[dimensionA-1][dimensionA-1];
+                Matriz det= new Matriz(dimensionA-1, dimensionA-1, false);
                 double detValor;
                 for(k=0;k<dimensionA;k++) {
                     if(k!=i) {
@@ -82,19 +111,20 @@ public class Matriz {
                             if(l!=j) {
                                 int indice1=k<i ? k : k-1 ;
                                 int indice2=l<j ? l : l-1 ;
-                                det[indice1][indice2]=a.datos[k][l];
+                                det.set_value_at(indice1, indice2, a.datos[k][l]);
+                                //det[indice1][indice2]=a.datos[k][l];
                             }
                         }
                     }
                 }
                 detValor=determinante(det);
-                nm.datos[i][j]=detValor * (double)Math.pow(-1, i+j+2);
+                nm.datos[i][j]= (int)(detValor * (double)Math.pow(-1, i+j+2));
             }
         }
         return nm;
     }
  
-    public Matriz matrizTranspuesta(Matriz a){
+    public static Matriz matrizTranspuesta(Matriz a){
         int i, j, dimensionA;
         dimensionA=a.getDimension().height;
         Matriz nuevam = new Matriz(dimensionA, dimensionA, false);
@@ -105,7 +135,7 @@ public class Matriz {
         return nuevam;
     }
  
-    public double determinante(Matriz a){
+    public static double determinante(Matriz a){
         int i, j, k, dimensionA;
         double det;
         dimensionA=a.getDimension().height;
@@ -114,9 +144,9 @@ public class Matriz {
             return det;
         }
         double suma=0;
-        for(i=0; i<dimensionA; i++){
+        for(i=0; i<dimensionA; i++){ 
         Matriz nm = new Matriz(dimensionA-1, dimensionA-1, false);
-            for(int j=0; j<dimensionA; j++){
+            for(j=0; j<dimensionA; j++){
                 if(j!=i){
                     for(k=1; k<dimensionA; k++){
                         int indice=-1;
@@ -133,21 +163,31 @@ public class Matriz {
             else
                 suma-=a.datos[i][0] * determinante(nm);
         }
+        System.out.println("det1: "+suma);  
         return suma;
     }
     
-    public static Matriz multiplicarDosMatrices(Matriz a, Matriz b) throws DimensionesIncompatibles {  
-        if(! a.getDimension()width.equals(b.getDimension().height)) throw new DimensionesIncompatibles("El número de columnas de A debe ser igual al número de filas de B ");
-        int i, j, filasA, columnasB;
+    
+    // Multiples A %*% B
+    public static Matriz multiplicarDosMatrices(Matriz b, Matriz a) throws DimensionesIncompatibles {  
+
+        
+        
+        if(b.getDimension().height != a.getDimension().width) throw new DimensionesIncompatibles("El número de columnas de A debe ser igual al número de filas de B ");
+        int i, j, filasA, columnasB, columnasA;
         filasA = a.getDimension().height;
         columnasB = b.getDimension().width;
         columnasA = a.getDimension().width;
+        
+        
         Matriz matrizResultante = new Matriz(filasA, columnasB, false);
+        
         for (i = 0; i < filasA; i++) {
             for (j = 0; j < columnasB; j++) {
-                matrizResultante.datos[i][j] = 0;
-                for (k = 0; k < columnasA; k++) {
-                    matrizResultante.datos[i][j] += a.datos[i][k] * b.datos[k][j];
+                matrizResultante.datos[j][i] = 0;
+                for (int k = 0; k < columnasA; k++) {
+                    
+                    matrizResultante.datos[j][i] += a.datos[k][i] * b.datos[j][k];
                 }
             }
         }
